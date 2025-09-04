c6248376f0a518ad2ec6e029ff34110f65b82d0b
Yuanyang Zhang <85085067+zhangyuanyang21@users.noreply.github.com>
2025-09-04 11:00:42 +0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -69,6 +69,68 @@ def get_default_config(data_name):
             n_clusters=21,
         ),
     )
+    elif data_name in ['Synthetic3d']:
+            return dict(
+                Autoencoder=dict(
+                    arch1=[3, 1024, 1024, 1024, 128],
+                    arch2=[3, 1024, 1024, 1024, 128],
+                    activations1='relu',
+                    activations2='relu',
+                    batchnorm=True,
+                ),
+                training=dict(
+                    seed=6,
+                    mask_seed=5,
+                    missing_rate=0.3,
+                    batch_size=256,
+                    epoch=200,
+                    lr=1.0e-4,
+                    lambda1=1,
+                    lambda2=0.1,
+                    n_clusters=3,
+                ),
+                diffusion=dict(
+                    emb_size=128,
+                    time_type="sinusoidal",
+                    out_dim1=128,
+                    out_dim2=128,
+                ),
+                noise_scheduler=dict(
+                    num_timesteps= 100,
+                    beta_schedule="linear",
+                ),
+            )
+    elif data_name in ['Multi-Fashion']:
+        return dict(
+            Autoencoder=dict(
+                arch1=[784, 1024, 1024, 1024, 128],
+                arch2=[784, 1024, 1024, 1024, 128],
+                activations1='relu',
+                activations2='relu',
+                batchnorm=True,
+            ),
+            training=dict(
+                missing_rate=0.3,
+                seed=2,
+                mask_seed=5,
+                batch_size=256,
+                epoch=200,
+                lr=1.0e-4,
+                lambda1=10.0,
+                lambda2=0.1,
+                n_clusters=10,
+            ),
+            diffusion=dict(
+                emb_size=128,
+                time_type="sinusoidal",
+                out_dim1=128,
+                out_dim2=128,
+            ),
+            noise_scheduler=dict(
+                num_timesteps=100,
+                beta_schedule="linear",
+            ),
+        )
     
     else:
         raise Exception('Undefined data_name')
