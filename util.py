adb392e2247d9bfe34ca795d1808c4bde297e746
Yuanyang Zhang <85085067+zhangyuanyang21@users.noreply.github.com>
2026-03-22 21:00:31 +0800

    Update util.py

--- util.py
+++ util.py
@@ -18,6 +18,29 @@ def next_batch(X1, X2, X3, X4, batch_size):
 
         yield (batch_x1, batch_x2, batch_x3, batch_x4, (i + 1))
 
+def cal_std(logger, *arg):
+    """ print clustering results """
+    if len(arg) == 3:
+        logger.info(arg[0])
+        logger.info(arg[1])
+        logger.info(arg[2])
+        output = """ 
+                     ACC {:.2f} std {:.2f}
+                     NMI {:.2f} std {:.2f} 
+                     ARI {:.2f} std {:.2f}""".format(np.mean(arg[0]) * 100, np.std(arg[0]) * 100, np.mean(arg[1]) * 100,
+                                                     np.std(arg[1]) * 100, np.mean(arg[2]) * 100, np.std(arg[2]) * 100)
+        logger.info(output)
+        output2 = str(round(np.mean(arg[0]) * 100, 2)) + ',' + str(round(np.std(arg[0]) * 100, 2)) + ';' + \
+                  str(round(np.mean(arg[1]) * 100, 2)) + ',' + str(round(np.std(arg[1]) * 100, 2)) + ';' + \
+                  str(round(np.mean(arg[2]) * 100, 2)) + ',' + str(round(np.std(arg[2]) * 100, 2)) + ';'
+        logger.info(output2)
+        return round(np.mean(arg[0]) * 100, 2), round(np.mean(arg[1]) * 100, 2), round(np.mean(arg[2]) * 100, 2)
+
+    elif len(arg) == 1:
+        logger.info(arg)
+        output = """ACC {:.2f} std {:.2f}""".format(np.mean(arg) * 100, np.std(arg) * 100)
+        logger.info(output)
+
 def normalize(x):
     """Normalize"""
     x = (x - np.min(x)) / (np.max(x) - np.min(x))
